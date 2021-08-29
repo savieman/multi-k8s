66982767331e28f78620d5690356214af803e709
Saviel <saviel@pop-os.localdomain>
2021-08-29 15:19:58 +0200

    fixed typos in deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,6 @@
 docker build -t savieman/multi-client:latest -t savieman/multi-client:$SHA -f ./client/Dockerfile ./client
-docker build -t savieman/mutli-server:latest -t savieman/mutli-server:$SHA -f ./server/Dockerfile ./server
-docker build -t savieman/mutli-worker:latest -t savieman/mutli-worker:$SHA -f ./worker/Dockerfile ./worker
+docker build -t savieman/multi-server:latest -t savieman/multi-server:$SHA -f ./server/Dockerfile ./server
+docker build -t savieman/multi-worker:latest -t savieman/multi-worker:$SHA -f ./worker/Dockerfile ./worker
 docker push savieman/multi-client:latest
 docker push savieman/multi-server:latest
 docker push savieman/multi-worker:latest
